70fada4a2f6ebd5afd34091116b5b354bd708586
paul88vera <paul88vera@gmail.com>
2022-05-04 23:39:55 -0500

    created header

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return(
+    <header>
+      <nav>
+        <h1><a href="/">Old Fashioned</a></h1>
+        <ul>
+          <li><a href="#">Gin</a></li>
+          <li><a href="#">Bourbon</a></li>
+          <li><a href="#">Vodka</a></li>
+        </ul>
+        <h3><a href="#">Login</a></h3>
+      </nav>
+    </header>
+  )
+}
+export default Header;
